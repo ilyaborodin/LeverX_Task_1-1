dc559abc005e81cf60addbdbb549a712196c68f0
bobrikilya <ilya.bobrik.2020@gmail.com>
2020-11-22 22:31:05 +0300

    Created function for unloading new_rooms info to json

---
+++ unloading.py
@@ -0,0 +1,15 @@
+import json
+
+
+class Unloading_info_to_json:
+    def __init__(self, info_list):
+        self.info_list = info_list
+
+    def unloading(self):
+        try:
+            with open("rooms_with_students.json", 'w') as new_file:
+                json.dump(self.info_list, new_file)
+        except:
+            print("Sorry, you failed")
+        finally:
+            return "Mission completed"
